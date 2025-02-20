49126ab24dcab2361143e76e3eeab84337529afa
flxwlff <felix.wolff@tu-dortmund.de>
2025-02-20 12:25:23 +0100

    chore: copy over more code from fusion2urdf

--- applications/cls-cad-fusion-plugin/commands/exporturdf/entry.py
+++ applications/cls-cad-fusion-plugin/commands/exporturdf/entry.py
@@ -1,6 +1,9 @@
 import re
 import shutil
 import unicodedata
+import fileinput
+from distutils.dir_util import copy_tree
+import sys
 
 import adsk.core
 
@@ -28,6 +31,108 @@ ICON_FOLDER = os.path.join(os.path.dirname(__file__), "resources", "")
 local_handlers = []
 progress_dialog: adsk.core.ProgressDialog = None
 export_path = ""
+
+def copy_package(save_dir, package_dir):
+    try: os.mkdir(save_dir + '/launch')
+    except: pass 
+    try: os.mkdir(save_dir + '/urdf')
+    except: pass 
+    copy_tree(package_dir, save_dir)
+
+def update_cmakelists(save_dir, package_name):
+    file_name = save_dir + '/CMakeLists.txt'
+
+    for line in fileinput.input(file_name, inplace=True):
+        if 'project(fusion2urdf)' in line:
+            sys.stdout.write("project(" + package_name + ")\n")
+        else:
+            sys.stdout.write(line)
+
+def update_package_xml(save_dir, package_name):
+    file_name = save_dir + '/package.xml'
+
+    for line in fileinput.input(file_name, inplace=True):
+        if '<name>' in line:
+            sys.stdout.write("  <name>" + package_name + "</name>\n")
+        elif '<description>' in line:
+            sys.stdout.write("<description>The " + package_name + " package</description>\n")
+        else:
+            sys.stdout.write(line)
+
+def copy_occs(root):    
+    """    
+    duplicate all the components
+    """    
+    def copy_body(allOccs, occs):
+        """    
+        copy the old occs to new component
+        """
+        
+        bodies = occs.bRepBodies
+        transform = adsk.core.Matrix3D.create()
+        
+        # Create new components from occs
+        # This support even when a component has some occses. 
+
+        new_occs = allOccs.addNewComponent(transform)  # this create new occs
+        if occs.component.name == 'base_link':
+            occs.component.name = 'old_component'
+            new_occs.component.name = 'base_link'
+        else:
+            new_occs.component.name = re.sub('[ :()]', '_', occs.name)
+        new_occs = allOccs.item((allOccs.count-1))
+        for i in range(bodies.count):
+            body = bodies.item(i)
+            body.copyToComponent(new_occs)
+    
+    allOccs = root.occurrences
+    oldOccs = []
+    coppy_list = [occs for occs in allOccs]
+    for occs in coppy_list:
+        if occs.bRepBodies.count > 0:
+            copy_body(allOccs, occs)
+            oldOccs.append(occs)
+
+    for occs in oldOccs:
+        occs.component.name = 'old_component'
+
+
+def export_stl(design, save_dir, components):  
+    """
+    export stl files into "sace_dir/"
+    
+    
+    Parameters
+    ----------
+    design: adsk.fusion.Design.cast(product)
+    save_dir: str
+        directory path to save
+    components: design.allComponents
+    """
+          
+    # create a single exportManager instance
+    exportMgr = design.exportManager
+    # get the script location
+    try: os.mkdir(save_dir + '/meshes')
+    except: pass
+    scriptDir = save_dir + '/meshes'  
+    # export the occurrence one by one in the component to a specified file
+    for component in components:
+        allOccus = component.allOccurrences
+        for occ in allOccus:
+            if 'old_component' not in occ.component.name:
+                try:
+                    print(occ.component.name)
+                    fileName = scriptDir + "/" + occ.component.name              
+                    # create stl exportOptions
+                    stlExportOptions = exportMgr.createSTLExportOptions(occ, fileName)
+                    stlExportOptions.sendToPrintUtility = False
+                    stlExportOptions.isBinaryFormat = True
+                    # options are .MeshRefinementLow .MeshRefinementMedium .MeshRefinementHigh
+                    stlExportOptions.meshRefinement = adsk.fusion.MeshRefinementSettings.MeshRefinementLow
+                    exportMgr.execute(stlExportOptions)
+                except:
+                    print('Component ' + occ.component.name + 'has something wrong.')
 
 def write_link_urdf(joints_dict, repo, links_xyz_dict, file_name, inertial_dict):
     """
@@ -158,11 +263,9 @@ def write_urdf(joints_dict, links_xyz_dict, inertial_dict, package_name, robot_n
         f.write('<?xml version="1.0" ?>\n')
         f.write('<robot name="{}" xmlns:xacro="http://www.ros.org/wiki/xacro">\n'.format(robot_name))
         f.write('\n')
-        f.write('<xacro:include filename="$(find {})/urdf/materials.xacro" />'.format(package_name))
-        f.write('\n')
-        f.write('<xacro:include filename="$(find {})/urdf/{}.trans" />'.format(package_name, robot_name))
-        f.write('\n')
-        f.write('<xacro:include filename="$(find {})/urdf/{}.gazebo" />'.format(package_name, robot_name))
+        f.write('<material name="silver">\n')
+        f.write('  <color rgba="0.700 0.700 0.700 1.000"/>\n')
+        f.write('</material>\n')
         f.write('\n')
 
     write_link_urdf(joints_dict, repo, links_xyz_dict, file_name, inertial_dict)
@@ -1034,6 +1137,7 @@ def command_execute(args: adsk.core.CommandEventArgs):
     # --------------------
     # Generate URDF
     write_urdf(joints_dict, links_xyz_dict, inertial_dict, package_name, robot_name, save_dir)
+    write_xacro(joints_dict, links_xyz_dict, inertial_dict, package_name, robot_name, save_dir)
     write_materials_xacro(joints_dict, links_xyz_dict, inertial_dict, package_name, robot_name, save_dir)
     write_transmissions_xacro(joints_dict, links_xyz_dict, inertial_dict, package_name, robot_name, save_dir)
     write_gazebo_xacro(joints_dict, links_xyz_dict, inertial_dict, package_name, robot_name, save_dir)
@@ -1041,6 +1145,15 @@ def command_execute(args: adsk.core.CommandEventArgs):
     write_gazebo_launch(package_name, robot_name, save_dir)
     write_control_launch(package_name, robot_name, save_dir, joints_dict)
     write_yaml(package_name, robot_name, save_dir, joints_dict)
+
+     # copy over package files
+    utils.copy_package(save_dir, package_dir)
+    utils.update_cmakelists(save_dir, package_name)
+    utils.update_package_xml(save_dir, package_name)
+
+    # Generate STl files        
+    copy_occs(root)
+    export_stl(design, save_dir, components) 
         
 
 
